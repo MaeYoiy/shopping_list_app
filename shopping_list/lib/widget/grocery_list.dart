6c342ec9c9869687bba1122721b60e871769fac6
MaeYoiy <b6333034@gmail.com>
2023-12-19 21:45:10 +0700

    POST Request and Fetching and Loading state

--- shopping_list/lib/widget/grocery_list.dart
+++ shopping_list/lib/widget/grocery_list.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widget/new_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,7 +13,45 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-7b448-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    // print(response.body);
+    // decode = ถอดรหัส
+    // listData type map มีค่า (key = String, value = Map<String, dynamic>)
+    // และใน Map มีค่า (key = String, value = dynamic) ที่เป็นแบบ dynamic ก็เพราะว่า มี value ที่เป็น 2 แบบคือ String ("Vegetables", "mink") และ int (12)
+    // รูปแบบ Map เป็น แบบนี้ {"-Nlr476qqFM0X-2UFp7R":{"category":"Vegetables","name":"mink","quantity":12},
+    // final Map<String, Map<String, dynamic>> listData = json.decode(response.body);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadItems = [];
+    for (final item in listData.entries) {
+      // firstWhere คือเอาตัวแรกที่เจอ
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      loadItems.add(
+        GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category),
+      );
+    }
+    setState(() {
+      _groceryItems = loadItems;
+    });
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -31,6 +72,7 @@ class _GroceryListState extends State<GroceryList> {
   void _removeItem(GroceryItem item) {
     setState(() {
       _groceryItems.remove(item);
+      _isLoading = false;
     });
   }
 
@@ -39,6 +81,12 @@ class _GroceryListState extends State<GroceryList> {
     Widget content = const Center(
       child: Text('No items added yet,'),
     );
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
--- shopping_list/lib/widget/new_item.dart
+++ shopping_list/lib/widget/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -17,21 +20,65 @@ class _NewItem extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
-  void _saveItem() {
+  void _saveItem() async {
     //ถ้าเป็นจริงจะทำการ Save
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
+      final url = Uri.https('flutter-prep-7b448-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+
+      final response = await http.post(
+        url,
+        // headers เป็นรูปแบบ json
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // body เป็นข้อมูลการ save item ที่เราได้ add ไปโดยเรา encode เพื่อแปลง body เป็น json ซึ่งเราจะใช้ map
+        // ซึ่ง firebase จะ generate 'id' ให้เราเอง
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
+
+      print(response.body);
+      print(response.statusCode);
+
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      if (context.mounted) {
+        Navigator.of(context).pop(
+          GroceryItem(
+              id: resData['name'],
+              name: _enteredName,
+              quantity: _enteredQuantity,
+              category: _selectedCategory),
+        );
+      }
+
+      // if (!context.mounted) {
+      //   return;
+      // }
+      // Navigator.of(context).pop();
+
       // print(_enteredName);
       // print(_enteredQuantity);
       // print(_selectedCategory);
-      Navigator.of(context).pop(
-        GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory),
-      );
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //       id: DateTime.now().toString(),
+      //       name: _enteredName,
+      //       quantity: _enteredQuantity,
+      //       category: _selectedCategory),
+      // );
     }
   }
 
@@ -128,14 +175,22 @@ class _NewItem extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,
-                    child: const Text('Add Item'),
+                    onPressed: _isSending ? null : _saveItem,
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : const Text('Add Item'),
                   ),
                 ],
               )
